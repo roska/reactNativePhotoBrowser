8f54ac31aee18e078d92b11a9461e8bb8b8bd9c6
roska <juharai@gmail.com>
2019-01-13 22:11:22 +0200

    wip

--- App.js
+++ App.js
@@ -8,6 +8,8 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, ScrollView, Image} from 'react-native';
+import { Container, Header, Body, Title, Content, Card, CardItem } from 'native-base'
+import { Col, Row, Grid  } from 'react-native-easy-grid'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -27,6 +29,14 @@ async function getPhotos() {
   }
 }
 
+function splitArrayToChunks(array, chunkSize) {
+  let result = []
+  for (let i = 0; i < array.length; i++) {
+    result.push(array.slice(i, i + chunkSize))
+  }
+  return result
+}
+
 type Props = {};
 export default class App extends Component<Props> {
 
@@ -41,18 +51,23 @@ export default class App extends Component<Props> {
 
   render() {
     const { photos } = this.state
+    const foo = splitArrayToChunks(photos, 3)
+    console.log(foo)
     return (
-      <ScrollView style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <Container>
+        <Header>
+          <Body>
+            <Title>Foobar</Title>
+          </Body>
+        </Header>
+        <Content padder>
+          <Text>Täsä ny o kontenttii</Text>
 
-        <View style={styles.imageContainer}>
-        {
-          photos.map(photo => {
-            return (
-              <View style={styles.image}>
-                <Text>{photo.id}</Text>
+          <View style={styles.imageContainer}>
+          {
+            photos.map(photo => {
+              return (
+                <View style={styles.image}>
                   <Image
                     style={{height: 100, width: 100, borderRadius: 3}}
                     source={{uri: photo.thumbnailUrl}}
@@ -63,7 +78,8 @@ export default class App extends Component<Props> {
           }
           </View>
 
-      </ScrollView>
+        </Content>
+      </Container>
     );
   }
 }
@@ -78,26 +94,26 @@ const styles = StyleSheet.create({
     marginBottom: 25,
     padding: 15
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
   image: {
     // backgroundColor: 'cyan',
-    padding: 10,
+    padding: 5,
     // margin: 5,
     flexShrink: 1,
     width: '33%',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84,
+    elevation: 3,
   },
   imageContainer: {
     flex: 1,
     flexDirection: 'row',
     flexWrap: 'wrap',
+    justifyContent: 'center',
   }
 });
