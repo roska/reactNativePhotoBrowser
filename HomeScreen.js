24c13576c725dec5fa0901fa24b3c34d9218c606
roska <juharai@gmail.com>
2019-01-14 11:21:38 +0200

    get photos

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,13 +1,35 @@
 import React from 'react'
-import { View, Button } from 'react-native'
+import { View, Button, Text } from 'react-native'
+
+async function getPhotos() {
+  try {
+    let res = await fetch('http://jsonplaceholder.typicode.com/photos?_page=1&_limit=20')
+    if (!res.ok) throw Error(res.statusText)
+    return res.json()
+  } catch(error) {
+    console.log(error)
+  }
+}
 
 class HomeScreen extends React.Component {
+  state = {
+    photos: []
+  }
+
   static navigationOptions = {
     title: 'Welcome',
   };
 
+  async componentDidMount() {
+    const json = await getPhotos()
+    this.setState({photos: json})
+  }
+
   render() {
-    const {navigate} = this.props.navigation;
+    const { photos } = this.state
+    console.log(photos)
+    const { navigate } = this.props.navigation;
+
     return (
       <View>
         <Button
@@ -18,6 +40,9 @@ class HomeScreen extends React.Component {
           title="HomeScreen 2."
           onPress={() => navigate('Image', {})}
         />
+        {
+          photos.map(photo => <Text>{photo.id}</Text>)
+        }
       </View>
     );
   }
